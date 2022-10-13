9bd3e9b3c20f36f5b4517134cc71ea8b357f2138
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-14 00:36:26 +0530

    Short Url Get link done

--- controllers/shortUrlController.js
+++ controllers/shortUrlController.js
@@ -1,3 +1,4 @@
+const url = require("url");
 const shortId = require("shortid");
 const validUrl = require("valid-url");
 const ShortUrl = require("./../models/ShortUrl");
@@ -26,13 +27,65 @@ let shortUrlCreate = async (req, res, next) => {
               status: true,
               shortId: data.shortId,
               fullUrl: data.fullUrl,
-              message: "Short Link created successfully",
+              message: "Short Link found successfully",
             });
           });
         }
       });
     } catch (err) {
-      console.log(err);
+      return res.json({
+        status: false,
+        message: "Something went wrong",
+      });
+    }
+  } else {
+    return res.json({
+      status: false,
+      message: "Please Enter a Valid URL",
+    });
+  }
+};
+
+let shortUrlGet = async (req, res, next) => {
+  let link = req.body.url;
+  if (link !== null && validUrl.isUri(link)) {
+    try {
+      let parseUrl = url.parse(link);
+      let shortCode = parseUrl.pathname.slice(1);
+
+      if (!shortId.isValid(shortCode)) {
+        return res.json({
+          status: false,
+          message: "The link is not exist",
+        });
+      }
+
+      ShortUrl.findOne({ shortId: shortCode }, (err, short) => {
+        if (err) return next(err);
+
+        if (short) {
+          ShortUrl.findByIdAndUpdate(
+            short._id,
+            { $inc: { hitCount: 1 } },
+            { new: true },
+            (err, update) => {
+              return res.json({
+                status: true,
+                shortId: update.shortId,
+                fullUrl: update.fullUrl,
+                hitCount: update.hitCount,
+                message: "Short Link created successfully",
+              });
+            }
+          );
+        } else {
+          return res.json({
+            status: false,
+            message: "The link is not exist",
+          });
+        }
+      });
+    } catch (err) {
       return res.json({
         status: false,
         message: "Something went wrong",
@@ -48,4 +101,5 @@ let shortUrlCreate = async (req, res, next) => {
 
 module.exports = {
   shortUrlCreate: shortUrlCreate,
+  shortUrlGet: shortUrlGet,
 };
