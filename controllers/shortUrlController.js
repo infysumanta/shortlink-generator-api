08bec8a3f62deff18c1dd4abd92134ff37c5cf2a
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-13 12:39:11 +0530

    created shortId and saved in models

--- controllers/shortUrlController.js
+++ controllers/shortUrlController.js
@@ -1,13 +1,23 @@
 const shortId = require("shortid");
 const validUrl = require("valid-url");
+const ShortUrl = require("./../models/ShortUrl");
 
-let shortUrlCreate = async (req, res) => {
+let shortUrlCreate = async (req, res, next) => {
   let link = req.body.url;
   if (link !== null && validUrl.isUri(link)) {
     try {
-      return res.json({
-        status: true,
-        shortLink: shortId.generate(),
+      let body = {
+        shortId: shortId.generate(),
+        fullUrl: link,
+      };
+      ShortUrl.create(body, (err, data) => {
+        if (err) return next(err);
+        return res.json({
+          status: true,
+          shortId: data.shortId,
+          fullUrl: data.fullUrl,
+          message: "Short Link created successfully",
+        });
       });
     } catch (err) {
       console.log(err);
